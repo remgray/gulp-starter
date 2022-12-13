2f7e1166952fafb2f96340225cf2c29ef696de88
remgray <v.greygrayrem@gmail.com>
2022-12-13 03:26:26 +0200

    fix js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
-import { smoothScroll } from './libs/smoothScroll.js';
-import { optimizedResize } from './libs/optimizedResize.js';
+import smoothScroll from './libs/smoothScroll';
 import AOS from 'aos';
 // import './modules/menu.js'
 
@@ -7,7 +6,7 @@ import AOS from 'aos';
 document.addEventListener('DOMContentLoaded', () => {
 
   // Smooth scroll init
-  let scroll = new SmoothScroll('a[scroll]', {
+  let scroll = new smoothScroll('a[scroll]', {
     offset: function (anchor, toggle) {
       if (toggle.getAttribute("href") === '#hero') {
         return 90;
@@ -25,12 +24,12 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // change top margin for header height on ref click 
-  optimizedResize.add(function () {
-    let scroll = new SmoothScroll('a[scroll]', {
+  window.addEventListener('resize', () => {
+    let scroll = new smoothScroll('a[scroll]', {
       header: 'header',
       // offset: 60, // if offset another of header
       updateURL: false,
     });
-  });
+  })
 });
 
