95332192526a57736b74a8abe8b1d21022999bb3
remgray <v.greygrayrem@gmail.com>
2022-12-17 05:26:09 +0200

    fixed menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,23 @@
 import smoothScroll from './libs/smoothScroll';
 import AOS from 'aos';
-// import './modules/menu.js'
+// import burgerMenu from './modules/menu.js'
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
-  // Smooth scroll init
-  let scroll = new smoothScroll('a[scroll]', {
-    offset: function (anchor, toggle) {
-      if (toggle.getAttribute("href") === '#hero') {
-        return 90;
-      } else {
-        return 60;
-      }
-
-    },
-    updateURL: false,
-  });
-
   // Animate on scroll init
   AOS.init({
     once: true,
   });
 
-  // change top margin for header height on ref click 
-  window.addEventListener('resize', () => {
+
+  const smoothScrollPage = () => {
     let scroll = new smoothScroll('a[scroll]', {
       header: 'header',
       // offset: 60, // if offset another of header
       updateURL: false,
     });
-  })
+  }
+  
+  smoothScrollPage()
 });
 
--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,53 +1,57 @@
-import { showScroll, hideScroll } from './sidebarSwitcher.js'
+import { showScroll, hideScroll } from './sidebarSwitcher'
 
-// Menu selectors
-const menu = document.querySelector('.js-menu'),
-closeMenuBtns = document.querySelectorAll('.js-close'),
-burger = document.querySelector('.burger')
+const burgerMenu = () => {
+  // Menu selectors
+  const menu = document.querySelector('.js-menu'),
+    closeMenuBtns = document.querySelectorAll('.js-close'),
+    burger = document.querySelector('.burger')
 
-// Reset all classes
-const resetNav = () => {
-  menu.classList.remove('menu-open');
-  burger.classList.remove('active');
-  showScroll();
-}
-
-// Reset all classes on resize
-window.addEventListener('resize', () => {
-  if (menu.classList.contains('menu-open')){
-    resetNav()
+  // Reset all classes
+  const resetNav = () => {
+    menu.classList.remove('menu-open');
+    burger.classList.remove('active');
+    showScroll();
   }
-});
 
-// Show/Hide scroll on burger click
-burger.addEventListener('click', () => {
-  if (burger.classList.contains('active')) {
-    hideScroll()
-  } else {
-    showScroll()
-  }
-})
+  // Reset all classes on resize
+  window.addEventListener('resize', () => {
+    if (menu.classList.contains('menu-open')) {
+      resetNav()
+    }
+  });
 
-// Open/Hide menu on click
-function classToggleHandler() {
-  burger.classList.toggle('active');
-  menu.classList.toggle('menu-open');
-  if (menu.classList.contains('menu-open')) {
-    hideScroll()
-  } else {
-    showScroll()
-  }
-  return
-}
-
-// Toggle burger class
-burger.addEventListener('click', () => {
-  classToggleHandler();
-})
-
-// Reset all classes on click js-close buttons
-closeMenuBtns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    resetNav()
+  // Show/Hide scroll on burger click
+  burger.addEventListener('click', () => {
+    if (burger.classList.contains('active')) {
+      hideScroll()
+    } else {
+      showScroll()
+    }
   })
-})
+
+  // Open/Hide menu on click
+  function classToggleHandler() {
+    burger.classList.toggle('active');
+    menu.classList.toggle('menu-open');
+    if (menu.classList.contains('menu-open')) {
+      hideScroll()
+    } else {
+      showScroll()
+    }
+    return
+  }
+
+  // Toggle burger class
+  burger.addEventListener('click', () => {
+    classToggleHandler();
+  })
+
+  // Reset all classes on click js-close buttons
+  closeMenuBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      resetNav()
+    })
+  })
+}
+
+export default burgerMenu
