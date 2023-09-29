d0632264e2b22ee90a7147e6808e6f9f9ad0aef1
remgray <v.greygrayrem@gmail.com>
2023-09-29 15:44:43 +0300

    update template

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -2,28 +2,32 @@ import webpack from "webpack-stream";
 import sourcemaps from 'gulp-sourcemaps';
 
 export const js = () => {
-  return app.gulp.src(app.path.src.js)
-    .pipe(webpack({
-      mode: app.isBuild ? "production" : "development",
-      output: {
-        filename: 'app.min.js'
-      },
-      module: {
-        rules: [{
-          test: /\.m?js/,
-          type: "javascript/auto",
-        },
-        {
-          test: /\.m?js/,
-          resolve: {
-            fullySpecified: false,
-          },
-        },
-        ],
-      },
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write('../js'))
-    .pipe(app.gulp.dest(app.path.build.js))
-    .pipe(app.plugins.browserSync.stream());
+	return app.gulp.src(app.path.src.js)
+		.pipe(webpack({
+			mode: app.isBuild ? "production" : "development",
+			output: {
+				filename: 'app.min.js'
+			},
+			module: {
+				rules: [{
+					test: /\.m?js/,
+					type: "javascript/auto",
+				},
+				{
+					test: /\.m?js/,
+					resolve: {
+						fullySpecified: false,
+					},
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+				],
+			},
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write('../js'))
+		.pipe(app.gulp.dest(app.path.build.js))
+		.pipe(app.plugins.browserSync.stream());
 }
