100ead58430d4f226e28ca90f4704bfd3341c6b6
remgray <v.greygrayrem@gmail.com>
2023-09-29 15:48:38 +0300

    update template

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,4 @@
-import AOS from 'aos';
-import smoothScroll from './libs/smoothScroll';
-
 document.addEventListener('DOMContentLoaded', () => {
-	// Animate on scroll init
-	AOS.init({
-		once: true,
-	});
-	// scrolling
-	new smoothScroll('a[href^="#"]', {
-		// header: 'header',
-		offset: 0, // if offset another of header
-		updateURL: false,
-	});
+
 });
 
